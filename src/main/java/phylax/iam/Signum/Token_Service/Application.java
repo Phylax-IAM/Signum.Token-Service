161394b6a528195a3ef87a57792350f350063415
Pragyanshu Rai <pragyanshu@Pragyanshus-MacBook-Pro.local>
2025-08-21 21:38:29 +0530

    fix: made the app the base directory

--- src/main/java/phylax/iam/Signum/Token_Service/Application.java
+++ src/main/java/phylax/iam/Signum/Token_Service/Application.java
@@ -1,13 +1,13 @@
-package phylax.iam.signum;
+package phylax.iam.Signum.Token_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SignumApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SignumApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
