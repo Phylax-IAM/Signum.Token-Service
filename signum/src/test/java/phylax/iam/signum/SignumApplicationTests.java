16cc2c6c61d08c9f6bdaebe475eb6d8da59e3355
Pragyanshu Rai <pragyanshu@Pragyanshus-MacBook-Pro.local>
2025-08-21 19:02:24 +0530

    feat: added the application structure, populated and tested the application to make sure it is able to run and it is.

---
+++ signum/src/test/java/phylax/iam/signum/SignumApplicationTests.java
@@ -0,0 +1,13 @@
+package phylax.iam.signum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
