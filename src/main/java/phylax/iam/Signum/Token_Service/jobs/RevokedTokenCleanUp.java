25d3f9a12c23a0dd137229a60f24f3d504ef967e
Pragyanshu Rai <pragyanshu@Pragyanshus-MacBook-Pro.local>
2025-08-24 21:58:22 +0530

    fix: added a skeleton of the revokedTokenCleanUp file

--- src/main/java/phylax/iam/Signum/Token_Service/jobs/RevokedTokenCleanUp.java
+++ src/main/java/phylax/iam/Signum/Token_Service/jobs/RevokedTokenCleanUp.java
@@ -1,7 +1,7 @@
 package phylax.iam.Signum.Token_Service.jobs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class RevokedTokenCleanUp {
@@ -9,5 +9,5 @@ public class RevokedTokenCleanUp {
     @Value("${scheduler.revoked-token-cleanup.seconds:300}")
     private Integer intervalInSeconds;
 
-
+    // todo: add the logic for deleting the expired revoked token
 }
